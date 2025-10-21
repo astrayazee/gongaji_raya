4477be43be6723914e2fc7b2451722ebee8c93f3
ya? <hyuckraya@gmail.com>
2025-10-21 20:46:18 +0700

    add mobile menu toggle functionality

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  const hamburger = document.getElementById("hamburger");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const closeBtn = document.getElementById("closeBtn");
+
+  hamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+  });
