e0e5a285380b8d440775ed57e2053c0e77b60b24
ya? <hyuckraya@gmail.com>
2025-10-22 15:16:51 +0700

    refactor mobile menu and add scroll event listener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
   const hamburger = document.getElementById("hamburger");
   const mobileMenu = document.getElementById("mobileMenu");
   const closeBtn = document.getElementById("closeBtn");
@@ -9,3 +10,18 @@
   closeBtn.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
   });
+});
+
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  const logo = document.getElementById("navbar-logo");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+    logo.src = "https://gongaji.id/assets/images/gongaji-coloured.svg";
+  } else {
+    navbar.classList.remove("scrolled");
+    logo.src = "assets/icons/gongaji-white.svg";
+  }
+});
+
